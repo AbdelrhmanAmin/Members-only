8257210e3e403157de7454139a95c2a82a9588c5
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-10 13:11:13 +0600

    fixed errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   def signed_in_only!
     # current_user in ApplicationController must be defined as helper method to make it work
-    redirect_to  new_user_session_path unless current_user
+    redirect_to new_user_session_path unless current_user
   end
 
   # GET /posts
